06e2329ea7c45cfb4ee8088e4ac88bc8a9909250
Todyzinmaker <78418127+Todyzinmaker@users.noreply.github.com>
2021-02-02 09:18:00 -0300

    Add files via upload

---
+++ src/paulomenu.js
@@ -0,0 +1,13 @@
+const paulomenu = (prefix, pushname) => {
+    return `◪ *Comandos do Tody*
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃🔰─ ✔️ ${prefix}setprefix
+┃🔰─ ✔️ ${prefix}block
+┃🔰─ ✔️ ${prefix}bc
+┃🔰─ ✔️ ${prefix}bcgc
+┃🔰─ ✔️ ${prefix}clearall`
+┗━━━━━━━━━━━━━━━━━━━━┛
+
+}
+
+exports.paulomenu = paulomenu
